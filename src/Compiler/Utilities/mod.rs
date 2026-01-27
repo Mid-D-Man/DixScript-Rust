66c417bd4a62956f8bbdc54b1677cc17439c6118
Abdulhamid Manman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-01-27 15:06:28 +0100

    Update mod.rs

--- src/Compiler/Utilities/mod.rs
+++ src/Compiler/Utilities/mod.rs
@@ -1,8 +1,9 @@
 // src/Compiler/Utilities/mod.rs (add to existing)
 pub mod identifier_pattern_analyzer;
-
+mod security_utilities;  
 pub use identifier_pattern_analyzer::{
     IdentifierPatternAnalyzer,
     IdentifierPattern,
     IdentifierPatternType,
 };
+pub use security_utilities::SecurityUtilities;  

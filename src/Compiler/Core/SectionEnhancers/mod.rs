3c3e5475dec13ef71ee5d7772263cd78b2d4acd9
Abdulhamid Manman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-01-30 19:51:32 +0100

    Update mod.rs

--- src/Compiler/Core/SectionEnhancers/mod.rs
+++ src/Compiler/Core/SectionEnhancers/mod.rs
@@ -1,2 +1,12 @@
+// src/Compiler/Core/SectionEnhancers/mod.rs
 //! AST enhancers for different sections
-// TODO: Implement QuickFunctionsAstEnhancer, etc.
+
+pub mod qualified_identifier_resolution;
+pub mod qualified_identifier_resolver;
+pub mod quickfuncs_ast_enhancer;
+
+pub use qualified_identifier_resolution::{
+    QualifiedIdentifierKey, QualifiedIdentifierResolution, QualifiedIdentifierType,
+};
+pub use qualified_identifier_resolver::QualifiedIdentifierResolver;
+pub use quickfuncs_ast_enhancer::QuickFunctionsAstEnhancer;

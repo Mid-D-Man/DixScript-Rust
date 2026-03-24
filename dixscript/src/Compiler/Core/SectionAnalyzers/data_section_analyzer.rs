e2d5e4bd0baa112a8d7af4a541413ef976b39ffc
Abdulhamid Manman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-03-24 17:07:18 +0100

    Update data_section_analyzer.rs

--- dixscript/src/Compiler/Core/SectionAnalyzers/data_section_analyzer.rs
+++ dixscript/src/Compiler/Core/SectionAnalyzers/data_section_analyzer.rs
@@ -72,7 +72,16 @@ impl<'a> DataSectionAnalyzer<'a> {
             path_to_type:             FxHashMap::default(),
         }
     }
-
+pub fn new_with_error_manager(
+    operational_settings: &'a OperationalSettings,
+    error_manager: ErrorManager,
+) -> Self {
+    DataSectionAnalyzer {
+        error_manager,
+        debug_config: DebugConfig::from_debug_mode(operational_settings.debug_mode),
+        operational_settings,
+    }
+}
     pub fn analyze(
         &mut self,
         section: &DataSection,

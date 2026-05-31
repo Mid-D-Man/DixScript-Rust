b7ee8356c05ace82112545523f51316930a29c4a
AbdulHamid Mamman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-05-31 18:32:03 +0100

    Update mod.rs

--- dixscript/src/Builtins/Instance/mod.rs
+++ dixscript/src/Builtins/Instance/mod.rs
@@ -7,7 +7,8 @@ pub mod tuple_methods;
 pub mod universal_methods;
 pub mod regex_methods;
 pub mod blob_methods;
-pub mod datetime_instance_methods;          // ← new
+pub mod datetime_instance_methods;
+pub mod object_methods;
 
 pub use number_methods::{
     get_int_methods,
@@ -15,13 +16,14 @@ pub use number_methods::{
     get_float_methods,
     get_double_methods,
 };
-pub use string_methods::get_methods   as get_string_methods;
-pub use array_methods::get_methods    as get_array_methods;
-pub use tuple_methods::get_methods    as get_tuple_methods;
+pub use string_methods::get_methods    as get_string_methods;
+pub use array_methods::get_methods     as get_array_methods;
+pub use tuple_methods::get_methods     as get_tuple_methods;
 pub use universal_methods::get_methods as get_universal_methods;
-pub use regex_methods::get_methods    as get_regex_methods;
-pub use blob_methods::get_methods     as get_blob_methods;
-pub use datetime_instance_methods::{        // ← new
-                                            get_timestamp_methods,
-                                            get_date_methods,
+pub use regex_methods::get_methods     as get_regex_methods;
+pub use blob_methods::get_methods      as get_blob_methods;
+pub use datetime_instance_methods::{
+    get_timestamp_methods,
+    get_date_methods,
 };
+pub use object_methods::get_methods    as get_object_methods;

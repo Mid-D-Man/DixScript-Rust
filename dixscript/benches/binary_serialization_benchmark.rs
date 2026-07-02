c7beaa3003d5b37266f195045576cf9b8d0c7c7f
AbdulHamid Mamman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-07-02 12:07:23 +0100

    Update binary_serialization_benchmark.rs

--- dixscript/benches/binary_serialization_benchmark.rs
+++ dixscript/benches/binary_serialization_benchmark.rs
@@ -19,7 +19,7 @@ use criterion::{
 };
 use bzip2::{write::BzEncoder, Compression as BzCompression};
 use flate2::{write::GzEncoder, Compression as GzCompression};
-use lzma_rust2::XzWriter;
+use lzma_rust2::{XzWriter, XzOptions};
 use serde::{Deserialize, Serialize};
 use std::io::Write;
 use std::sync::Once;
@@ -237,7 +237,7 @@ fn compress_bzip2(data: &[u8]) -> Vec<u8> {
 }
 
 fn compress_lzma(data: &[u8]) -> Vec<u8> {
-    let mut enc = XzWriter::new(Vec::with_capacity(data.len()), 6);
+    let mut enc = XzWriter::new(Vec::with_capacity(data.len()), XzOptions::default()).unwrap();
     enc.write_all(data).unwrap();
     enc.finish().unwrap()
 }
